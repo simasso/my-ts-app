77029a9ed5846eeeafab3ad728b7b0c31ecfaef8
Simone Massonie <simone.massonie@gmail.com>
2025-12-28 12:07:32 +0100

    add utils

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,11 @@
-const message = "Hello from TypeScript!";
-console.log(message);
+import { pickRandom } from "#utils";
+
+console.log(pickRandom([1, 2, 3, 4, 5]));
+console.log(pickRandom(["apple", "banana", "cherry"]));
+console.log(pickRandom([]));
+console.log(pickRandom([true, false, true]));
+console.log(pickRandom(["red", "green", "blue", "yellow"]));
+console.log(pickRandom([null, undefined, 42]));
+console.log(pickRandom(["a", "b", "c", "d", "e", "f", "g"]));
+console.log(pickRandom([1.1, 2.2, 3.3, 4.4, 5.5]));
+console.log(pickRandom(["x", "y", "z", "w", "v"]));
